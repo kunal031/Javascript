8fc6d14dbfbe1f6718728f006eab27f0b6cb446d
Kunal Raj <68905773+kunal031@users.noreply.github.com>
2025-09-30 15:17:00 +0000

    cotainer config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "kunal@gmail.com"
+var accountPassword = "123456"
+accountCity = "Patna"
+let accountState;
+
+console.log(accountId)
+
+
+/*
+    Prefer not to use var
+    because of issue in block space annd functional space
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
