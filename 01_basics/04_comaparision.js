43b3ba639fec122762bfea439c1d81e4c5d78b33
Kunal Raj <68905773+kunal031@users.noreply.github.com>
2025-10-01 20:15:48 +0000

    comparisions

---
+++ 01_basics/04_comaparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+
+// console.log("2" > 1)
+
+/* 11-17 these types of comparisions are very confusing , so try to avoid it because value is always of clean code */
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
+// ===
+console.log("2" === 2)
