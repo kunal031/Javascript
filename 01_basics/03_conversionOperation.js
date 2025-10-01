84ec2f3fcf4d3188c16f23cdf8117fc5d4fc2970
Kunal Raj <68905773+kunal031@users.noreply.github.com>
2025-10-01 19:59:15 +0000

    Operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 
 // "33" => 33
@@ -15,7 +15,7 @@ console.log(valueInNumber)
 let isLoggedIn = 0
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -25,5 +25,45 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber) 
+
+
+// ********** Operations **********
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(9/3)     give decimal value if something after point
+// console.log(5%3)
+
+let str1 = "Hello"
+let str2 = " Kunal"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1 " + 2)
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(3 + 4 * 5 % 3)
+console.log(+true)
+console.log(1+null)
+console.log(1+undefined)
+console.log((2+""))
+console.log(typeof (2+""))
+
+
+let num1=5,num2,num3
+
+num1 = num2 = num3 = 2+2
+console.log(num1)
+
