1cafe5d11a87a71d3d954ccea0a065a77524e406
Kunal Raj <68905773+kunal031@users.noreply.github.com>
2025-10-01 18:23:46 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// try to make code readability high
+
+let name = "Kunal"
+let age = 18
+let isLoggedIn = false
+
+/* Premitive Datatypes */
+
+// number => 2 to power 53 (range)
+// bigint : when number is too big
+// string => single quote/double quote
+// boolean => true / false
+// null => also a datatype : standalone value
+// undefined => jb value assign nhi hui hogi
+// symbol => define uniqueness
+
+
+
+/*  Object  */
+
+console.log(typeof null)
+console.log(typeof undefined)
